03accd510c122ea469d64e143a423efc8952dc4e
Your Name <you@example.com>
2023-12-13 04:48:14 -0500

    working bookings update

--- pages/api/bookings/updateBooking/[bookingId].js
+++ pages/api/bookings/updateBooking/[bookingId].js
@@ -1,4 +1,4 @@
-import { client, run } from '../../../../lib/mongodb';
+import { client, run } from '../../../lib/mongodb';
 import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
