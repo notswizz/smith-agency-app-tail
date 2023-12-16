ca94a459a9b012e2df43717809c4626d791918c4
Your Name <you@example.com>
2023-12-16 02:32:46 -0500

    agentdata previous bookings

--- components/agent/AgentData.js
+++ components/agent/AgentData.js
@@ -1,38 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const AgentData = ({ agents, onDeleteAgent, agentAppearances }) => {
-    const formatLocation = (location) => {
-        return Array.isArray(location) ? location.join(', ') : location;
+const AgentData = ({ agents, onDeleteAgent }) => {
+    const [agentClients, setAgentClients] = useState({});
+    const [agentDaysWorked, setAgentDaysWorked] = useState({});
+
+    useEffect(() => {
+        const fetchBookings = async () => {
+            const response = await fetch('/api/bookings/getBookings');
+            if (response.ok) {
+                const bookings = await response.json();
+                processBookings(bookings);
+            }
+        };
+
+        fetchBookings();
+    }, []);
+
+    const processBookings = (bookings) => {
+        const agentToClientsMap = {};
+        const daysWorkedMap = {};
+
+        bookings.forEach(booking => {
+            booking.agentSelection.flat().forEach(agentName => {
+                if (agentName) {
+                    if (!agentToClientsMap[agentName]) {
+                        agentToClientsMap[agentName] = new Set();
+                        daysWorkedMap[agentName] = 0;
+                    }
+                    agentToClientsMap[agentName].add(booking.client);
+                    daysWorkedMap[agentName] += 1;
+                }
+            });
+        });
+
+        setAgentClients(convertSetsToArrays(agentToClientsMap));
+        setAgentDaysWorked(daysWorkedMap);
     };
 
-    const sortedAgents = agents.sort((a, b) => {
-        return (agentAppearances[b._id] || 0) - (agentAppearances[a._id] || 0);
-    });
+    const convertSetsToArrays = (setsMap) => {
+        const arraysMap = {};
+        Object.keys(setsMap).forEach(key => {
+            arraysMap[key] = Array.from(setsMap[key]);
+        });
+        return arraysMap;
+    };
 
     return (
         <div className="flex flex-col space-y-4 max-h-96 overflow-auto">
-            {sortedAgents.map(agent => (
+            {agents.map(agent => (
                 <div className="bg-white p-4 rounded shadow-md flex justify-between" key={agent._id}>
-                    <div> {/* Left column for agent details */}
-                
+                    <div>
                         <h3 className="text-lg font-bold">
                             <a href={`https://www.instagram.com/${agent.instagram}/`} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">{agent.name}</a>
                         </h3>
-                        <span className="inline-block bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
-                            {agentAppearances[agent._id] || 0} Days
+                         <span className="inline-block bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+                            {agentDaysWorked[agent.name] || 0} Days Worked
                         </span>
                         <p className="text-gray-600">{agent.email}</p>
                         <p className="text-gray-600">{agent.phone}</p>
-                        <p className="text-gray-600">{formatLocation(agent.location)}</p>
+                        <p className="text-gray-600">Location: {agent.location.join(', ')}</p>
                        
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Email</button>
-            
                         <button onClick={() => onDeleteAgent(agent._id)} className="mt-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Delete</button>
                     </div>
-                    <div> {/* Right column for notes */}
-                        {agent.notes && (
-                            <p className="text-gray-600">{agent.notes}</p>
-                        )}
+                    <div className="clients-list">
+                        <h4 className="font-bold">Bookings:</h4>
+                        <ul>
+                            {agentClients[agent.name] && agentClients[agent.name].map(client => (
+                                <li key={client}>{client}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             ))}
