c4f7e33a89b037a77f1e6cc268f1c7986590d0a3
Your Name <you@example.com>
2023-12-17 02:32:33 -0500

    availform

--- components/form/AvailabilityForm.js
+++ components/form/AvailabilityForm.js
@@ -1,44 +1,67 @@
 import React, { useState, useMemo } from 'react';
-import { toUTCDateString } from '../../lib/utils'; // Import the utility function
+import { toUTCDateString } from '../../lib/utils';
 
 const AvailabilityForm = ({ agents, shows, onAvailabilityAdded }) => {
     const [selectedAgent, setSelectedAgent] = useState('');
     const [selectedShow, setSelectedShow] = useState('');
-    const [isAvailable, setIsAvailable] = useState('');
-    const [notes, setNotes] = useState('');
-    const [submissionSuccess, setSubmissionSuccess] = useState(false);
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [selectedDays, setSelectedDays] = useState({});
+    const [showDateRange, setShowDateRange] = useState({ startDate: '', endDate: '' });
+    const [notes, setNotes] = useState(''); // Add this line to declare the notes state
+    const [submissionSuccess, setSubmissionSuccess] = useState(false);
 
+    // Function to generate a range of dates
     const generateDateRange = (start, end) => {
         if (!start || !end) return [];
-
-        let currentDate = new Date(toUTCDateString(start));
-        const endUTCDate = new Date(toUTCDateString(end));
+        let currentDate = new Date(start);
+        const endUTCDate = new Date(end);
         const dates = [];
 
         while (currentDate <= endUTCDate) {
             dates.push(currentDate.toISOString().split('T')[0]);
             currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
         }
-        
+
         return dates;
     };
 
+    // useMemo hook to create date checkboxes
     const dateCheckboxes = useMemo(() => {
         const dateRange = generateDateRange(startDate, endDate);
-        return dateRange.map((date) => ({
+        return dateRange.map(date => ({
             date,
             checked: selectedDays[date] || false,
         }));
     }, [startDate, endDate, selectedDays]);
 
+   // Handle show selection and update date range
+const handleShowSelection = (showId) => {
+    const selectedShow = shows.find(show => show._id === showId);
+    if (selectedShow) {
+        setShowDateRange({
+            startDate: selectedShow.startDate,
+            endDate: selectedShow.endDate
+        });
+        setSelectedShow(showId);
+    } else {
+        // Reset the selected show and date range if no show is found
+        setShowDateRange({ startDate: '', endDate: '' });
+        setSelectedShow('');
+    }
+};
 
+    // Handle date checkbox change
+    const handleDateCheckboxChange = (date) => {
+        setSelectedDays(prevSelectedDays => ({
+            ...prevSelectedDays,
+            [date]: !prevSelectedDays[date],
+        }));
+    };
+
+    // Handle form submission
     const handleSubmit = async (event) => {
         event.preventDefault();
-
-        // Constructing the availability array
         const availabilityArray = Object.entries(selectedDays)
             .filter(([date, isChecked]) => isChecked)
             .map(([date]) => date);
@@ -46,7 +69,7 @@ const AvailabilityForm = ({ agents, shows, onAvailabilityAdded }) => {
         const submissionData = {
             agent: selectedAgent,
             show: selectedShow,
-            availability: availabilityArray, // Use the constructed array
+            availability: availabilityArray,
             notes: notes
         };
 
@@ -60,31 +83,20 @@ const AvailabilityForm = ({ agents, shows, onAvailabilityAdded }) => {
             });
 
             if (response.ok) {
-                setSubmissionSuccess(true); // Show success notification
-                resetFormFields();
+                setSubmissionSuccess(true); //
+                // Reset form on successful submission
+                setSelectedAgent('');
+                setSelectedShow('');
+                setStartDate('');
+                setEndDate('');
+                setSelectedDays({});
+                setNotes('');
             } else {
-                console.error('Submission was not successful. Response:', await response.json());
+                console.error('Submission was not successful:', await response.json());
             }
         } catch (error) {
             console.error('Error occurred during form submission:', error);
         }
-    };
-
-    const handleDateCheckboxChange = (date) => {
-        setSelectedDays(prevSelectedDays => ({
-            ...prevSelectedDays,
-            [date]: !prevSelectedDays[date],
-        }));
-    };
-
-    const resetFormFields = () => {
-        setSelectedAgent('');
-        setSelectedShow('');
-        setIsAvailable('');
-        setNotes('');
-        setStartDate('');
-        setEndDate('');
-        setSelectedDays({}); // Resetting the selected days
     };
 
     return (
@@ -108,70 +120,76 @@ const AvailabilityForm = ({ agents, shows, onAvailabilityAdded }) => {
                 </select>
             </div>
 
-            {/* Show Dropdown */}
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="show">
-                    Show:
-                </label>
-                <select
-                    id="show"
-                    value={selectedShow}
-                    onChange={(e) => setSelectedShow(e.target.value)}
-                    className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                >
-                    <option value="" disabled>Select a show...</option> {/* Default option */}
-                    {shows.map(show => (
-                        <option key={show._id.$oid} value={show._id.$oid}>{show.id}</option>
-                    ))}
-                </select>
-            </div>
+         {/* Show Dropdown */}
+<div className="mb-4">
+    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="show">
+        Show:
+    </label>
+    <select
+        id="show"
+        value={selectedShow}
+        onChange={(e) => handleShowSelection(e.target.value)}
+        className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    >
+        <option value="" disabled>Select a show...</option>
+        {shows.map(show => (
+            <option key={show._id} value={show._id}>{show.id}</option> // Ensure the correct key and value are set
+        ))}
+    </select>
+</div>
 
            
 
 
-           
-             {/* Start Date Input */}
-             <div className="mb-4">
-                <label htmlFor="start-date" className="block text-gray-700 text-sm font-bold mb-2">
-                    Start Date:
-                </label>
-                <input 
-                    type="date" 
-                    id="start-date" 
-                    value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                    className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-            </div>
+{/* Start Date Input */}
+<div className="mb-4">
+    <label htmlFor="start-date" className="block text-gray-700 text-sm font-bold mb-2">
+        Start Date:
+    </label>
+    <input 
+        type="date" 
+        id="start-date" 
+        value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}
+        min={showDateRange.startDate || '2023-01-01'}
+        max={showDateRange.endDate || '2099-12-31'}
+        className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    />
+</div>
 
-            {/* End Date Input */}
-            <div className="mb-4">
-                <label htmlFor="end-date" className="block text-gray-700 text-sm font-bold mb-2">
-                    End Date:
-                </label>
-                <input 
-                    type="date" 
-                    id="end-date" 
-                    value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                    className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
-            </div>
+{/* End Date Input */}
+<div className="mb-4">
+    <label htmlFor="end-date" className="block text-gray-700 text-sm font-bold mb-2">
+        End Date:
+    </label>
+    <input 
+        type="date" 
+        id="end-date" 
+        value={endDate}
+        onChange={(e) => setEndDate(e.target.value)}
+        min={showDateRange.startDate || '2023-01-01'}
+        max={showDateRange.endDate || '2099-12-31'}
+        className="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    />
+</div>
+
+
     {/* Dynamically Created Checkboxes */}
-            <fieldset className="mb-4">
-                <legend className="block text-gray-700 text-sm font-bold mb-2">Select Days:</legend>
-                {dateCheckboxes.map(({ date, checked }) => (
-                    <label key={date} className="block">
-                        <input
-                            type="checkbox"
-                            checked={checked}
-                            onChange={() => handleDateCheckboxChange(date)}
-                            className="mr-2 leading-tight"
-                        />
-                        {date}
-                    </label>
-                ))}
-            </fieldset>
+             {/* Dynamically Created Checkboxes */}
+             <fieldset className="mb-4">
+                    <legend className="block text-gray-700 text-sm font-bold mb-2">Select Days:</legend>
+                    {dateCheckboxes.map(({ date, checked }) => (
+                        <label key={date} className="block">
+                            <input
+                                type="checkbox"
+                                checked={checked}
+                                onChange={() => handleDateCheckboxChange(date)}
+                                className="mr-2 leading-tight"
+                            />
+                            {date}
+                        </label>
+                    ))}
+                </fieldset>
 
  {/* Notes Section */}
  <div className="mb-4">
@@ -188,10 +206,13 @@ const AvailabilityForm = ({ agents, shows, onAvailabilityAdded }) => {
             </div>
             {/* Submit Button */}
             <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Submit
-            </button>
-            {/* Success Notification */}
-            {submissionSuccess && <div className="alert alert-success">Form submitted successfully!</div>}
+                    Submit
+                </button>
+
+                {/* Success Notification */}
+                {submissionSuccess && (
+                    <div className="alert alert-success">Form submitted successfully!</div>
+                )}
         </form>
         </div>
     );
