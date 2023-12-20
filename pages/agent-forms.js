6442656b1f0f6b78e83c7f12aead92a82e3dde82
Your Name <you@example.com>
2023-12-20 17:50:30 -0500

    pink form

--- pages/agent-forms.js
+++ pages/agent-forms.js
@@ -57,40 +57,41 @@ const FormsAgent = () => {
 
 
 
-   // Toggle between forms
-   const toggleForm = () => {
-    setIsAgentFormActive(!isAgentFormActive);
-};
+    const toggleForm = () => {
+        setIsAgentFormActive(!isAgentFormActive);
+    };
 
-return (
-    <>
-        <Header />
-        <div className="container mx-auto px-4 py-4">
-            <img src="/tsalogo.png" alt="TSA Logo" className="mx-auto mb-4 max-w-full h-auto" />
-            <button onClick={toggleForm} className="mb-4 bg-hot-pink-500 hover:bg-hot-pink-700 text-white font-bold py-2 px-4 rounded block w-full sm:w-auto">
-                {isAgentFormActive ? 'Switch to Availability Form' : 'Switch to New Agent Form'}
-            </button>
+    return (
+        <>
+            <Header />
+            <div className="container mx-auto px-4 py-4 bg-gray-100">
+                <img src="/tsalogo.png" alt="TSA Logo" className="mx-auto mb-4 max-w-full h-auto" />
+                <button 
+                    onClick={toggleForm} 
+                    className="mb-4 bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded block w-full sm:w-auto transition duration-300 ease-in-out">
+                    {isAgentFormActive ? 'Switch to Availability Form' : 'Switch to New Agent Form'}
+                </button>
 
-            {isAgentFormActive ? (
-                <div>
-                    <h2 className="text-center text-hot-pink-500 text-xl font-bold mb-4">New Agent Form</h2>
-                    <p className="text-white mb-4">Use this form to add new agents. Fill in the details like name, email, phone, and more.</p>
-                    <AgentFormAgent />
-                </div>
-            ) : (
-                <div>
-                    <h2 className="text-center text-hot-pink-500 text-xl font-bold mb-4">Availability Form</h2>
-                    <p className="text-white mb-4">This form is used to update the availability of agents for various shows. Select an agent, a show, and set the available dates.</p>
-                    <AvailabilityForm 
-                        agents={agents} 
-                        shows={shows} 
-                        onAvailabilityAdded={handleAvailabilityAdded} 
-                    />
-                </div>
-            )}
-        </div>
-    </>
-);
+                {isAgentFormActive ? (
+                    <div className="p-6 rounded-lg shadow-lg bg-white">
+                        <h2 className="text-center text-pink-600 text-xl font-bold mb-4">New Agent Form</h2>
+                        <p className="text-gray-600 mb-4">Use this form to add new agents. Fill in the details like name, email, phone, and more.</p>
+                        <AgentFormAgent />
+                    </div>
+                ) : (
+                    <div className="p-6 rounded-lg shadow-lg bg-white">
+                        <h2 className="text-center text-pink-800 text-xl font-bold mb-4">Availability Form</h2>
+                        <p className="text-gray-600 mb-4">This form is used to update the availability of agents for various shows. Select an agent, a show, and set the available dates.</p>
+                        <AvailabilityForm 
+                            agents={agents} 
+                            shows={shows} 
+                            onAvailabilityAdded={handleAvailabilityAdded} 
+                        />
+                    </div>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default FormsAgent;
