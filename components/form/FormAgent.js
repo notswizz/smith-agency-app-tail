cf489ba6f2ed18637d201e3ed64b78f5ce0d819e
swizz <emailswizz@gmail.com>
2024-01-04 17:01:28 -0500

    new agent form

--- components/form/FormAgent.js
+++ components/form/FormAgent.js
@@ -90,78 +90,161 @@ const AgentFormAgent = ({ onAgentAdded }) => {
 
     
     
-
     return (
         <div className="max-w-md mx-auto bg-white p-6 rounded shadow max-h-96 overflow-auto">
+            <h2 className="text-xl font-semibold mb-4">New Agent Profile</h2>
+            <p className="text-gray-600 mb-6">
+                Welcome to the agent onboarding form. Please complete the following details to create a new agent profile. Fields marked with an asterisk (*) are required.
+            </p>
+    
             {loading ? (
                 <div className="flex justify-center items-center">
-                
                     <p>{statusMessage}</p>
                 </div>
             ) : (
                 <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Name:</label>
-                    <input type="text" id="name" name="name" value={agent.name} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
-                    <input type="email" id="email" name="email" value={agent.email} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="phone" className="block text-gray-700 text-sm font-bold mb-2">Phone:</label>
-                    <input type="tel" id="phone" name="phone" value={agent.phone} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="location" className="block text-gray-700 text-sm font-bold mb-2">Location:</label>
-                    <select id="location" name="location" multiple value={agent.location} onChange={handleChange} className="shadow border rounded py-2 px-3 text-gray-700 w-full">
-                        <option value="ATL">ATL</option>
-                        <option value="NYC">NYC</option>
-                        <option value="LA">LA</option>
-                        <option value="DAL">DAL</option>
-                    </select>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="college" className="block text-gray-700 text-sm font-bold mb-2">College:</label>
-                    <input 
-                        type="text" 
-                        id="college" 
-                        name="college" 
-                        value={agent.college} 
-                        onChange={handleChange} 
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                    />
-                </div>
-
-                <div className="mb-4">
-                    <label htmlFor="shoeSize" className="block text-gray-700 text-sm font-bold mb-2">Shoe Size:</label>
-                    <input 
-                        type="text" 
-                        id="shoeSize" 
-                        name="shoeSize" 
-                        value={agent.shoeSize} 
-                        onChange={handleChange} 
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
-                    />
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="instagram" className="block text-gray-700 text-sm font-bold mb-2">Instagram:</label>
-                    <input type="text" id="instagram" name="instagram" value={agent.instagram} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-                </div>
-              
-                <div className="mb-4">
-                    <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">Image:</label>
-                    <input type="file" id="image" name="image" onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-                </div>
-                
-                {/* Conditionally render the submit button based on the loading state */}
-                {!loading && (
-                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Add Agent</button>
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+                            Name*:
+                        </label>
+                        <input 
+                            type="text" 
+                            id="name" 
+                            name="name" 
+                            value={agent.name} 
+                            onChange={handleChange} 
+                            required 
+                            placeholder="Full Name"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        />
+                    </div>
+    
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+                            Email*:
+                        </label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            name="email" 
+                            value={agent.email} 
+                            onChange={handleChange} 
+                            required 
+                            placeholder="Email Address"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        />
+                    </div>
+    
+                    <div className="mb-4">
+                        <label htmlFor="phone" className="block text-gray-700 text-sm font-bold mb-2">
+                            Phone*:
+                        </label>
+                        <input 
+                            type="tel" 
+                            id="phone" 
+                            name="phone" 
+                            value={agent.phone} 
+                            onChange={handleChange} 
+                            required 
+                            placeholder="Phone Number"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        />
+                    </div>
+    
+                    <div className="mb-4">
+                        <label htmlFor="location" className="block text-gray-700 text-sm font-bold mb-2">
+                            Location*:
+                        </label>
+                        <select 
+                            id="location" 
+                            name="location" 
+                            multiple 
+                            value={agent.location} 
+                            onChange={handleChange} 
+                            required 
+                            className="shadow border rounded py-2 px-3 text-gray-700 w-full"
+                        >
+                            <option value="ATL">Atlanta (ATL)</option>
+                            <option value="NYC">New York City (NYC)</option>
+                            <option value="LA">Los Angeles (LA)</option>
+                            <option value="DAL">Dallas (DAL)</option>
+                        </select>
+                        <small className="text-gray-500">Hold down the Ctrl (Windows) / Command (Mac) button to select multiple options.</small>
+                    </div>
+    
+                    <div className="mb-4">
+                        <label htmlFor="college" className="block text-gray-700 text-sm font-bold mb-2">
+                            College:
+                        </label>
+                        <input 
+                            type="text" 
+                            id="college" 
+                            name="college" 
+                            value={agent.college} 
+                            onChange={handleChange} 
+                            placeholder="College"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        />
+                    </div>
+    
+                    <div className="mb-4">
+                        <label htmlFor="shoeSize" className="block text-gray-700 text-sm font-bold mb-2">
+                            Shoe Size:
+                        </label>
+                        <input 
+                            type="text" 
+                            id="shoeSize" 
+                            name="shoeSize" 
+                            value={agent.shoeSize} 
+                            onChange={handleChange} 
+                            placeholder="Shoe Size"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        />
+                    </div>
+    
+                    <div className="mb-4">
+                        <label htmlFor="instagram" className="block text-gray-700 text-sm font-bold mb-2">
+                            Instagram Handle:
+                        </label>
+                        <input 
+                            type="text" 
+                            id="instagram" 
+                            name="instagram" 
+                            value={agent.instagram} 
+                            onChange={handleChange} 
+                            placeholder="@"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        />
+                    </div>
+    
+                    <div className="mb-4">
+                        <label htmlFor="image" className="block text-gray-700 text-sm font-bold mb-2">
+                            Profile Image:
+                        </label>
+                        <input 
+                            type="file" 
+                            id="image" 
+                            name="image" 
+                            onChange={handleChange} 
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                        />
+                        <small className="text-gray-500">Choose a clear, professional headshot if available.</small>
+                    </div>
+    
+                    {!loading && (
+                        <button 
+                            type="submit" 
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+                        >
+                            Add Agent
+                        </button>
                     )}
                 </form>
             )}
         </div>
     );
+    
+
 };
 
 export default AgentFormAgent;
